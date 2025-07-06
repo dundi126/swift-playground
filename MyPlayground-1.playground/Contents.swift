d75ab67bf2908f84695aa578b0d52e56aca30a14
dundi126 <dundi126@gmail.com>
2025-07-05 23:28:48 -0400

    initial commit

---
+++ MyPlayground-1.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "New Playground"
+
+var new = "this"
+print(new)
+
+ new = "is"
+print(new)
+
+ new = "new"
+print(new )
+
+
+var day = """
+A day 
+in life of
+Developer
+"""
+
+print(day)
+
+print(day.count)
+
+print(day.uppercased())
+
+print(day.lowercased())
+
+
+let filename="example.txt"
+
+print(filename.hasPrefix("example"))
+
+print(filename.hasSuffix(".txt"))
