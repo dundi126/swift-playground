c3ba50f33e2535429cb3631bbe77a2ddfeaa5b44
dundi126 <dundi126@gmail.com>
2025-07-22 21:15:06 -0400

    Day-18 : M8 - Protocols

--- MyPlayground-7.playground/Contents.swift
+++ MyPlayground-7.playground/Contents.swift
@@ -3,7 +3,32 @@ import UIKit
 var greeting = "Hello, playground"
 
 let opposites = ["Mario":"Wario","Luigi":"Waluigi"]
-let peachOpposite = opposites["peach"]
+if let peachOpposite = opposites["Peach"] {
+    print(
+        "Peach opposite \(peachOpposite)"
+    )
+}else{
+    print("No opposite found")
+}
 
+func square(number:Int) -> Int{
+    number * number
+}
 
+var number :Int? = 9
 
+if let unwrapnumber = number{
+    print(square(number: unwrapnumber))
+}
+
+func printSquare(number:Int?){
+    guard let number = number else{
+        print("Missing Input")
+        return
+    }
+    
+    print("\(number) * \(number) : \(number*number)")
+}
+
+ 
+printSquare(number: 9)
