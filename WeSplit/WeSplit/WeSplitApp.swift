fa1dbd5e0aa4e2044b3e154db5834bdd7e1b6a46
dundi126 <dundi126@gmail.com>
2025-07-30 12:30:12 -0400

    Initial Project Setup for learning SwiftUI

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dundi vignesh Gutti on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
